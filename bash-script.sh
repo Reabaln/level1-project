b1bae5be85750c6738650de0a9ce89ab6567d835
Reem Als <46527514+Reemns97@users.noreply.github.com>
2020-07-07 19:38:34 +0300

    Update bash-script.sh
    update script

--- bash-script.sh
+++ bash-script.sh
@@ -2,4 +2,5 @@
 sudo apt-get update &&
 sudo apt-get install docker.io docker-compose jq make vim curl &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-
+sudo groupadd docker
+sudo usermod -aG docker $USER
